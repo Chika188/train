8be1af86123c7d56c207c355558e9aa6e73c3bd7
zjq <chika3319@163.com>
2025-07-04 09:02:43 +0800

    chore: 更新 ESLint 配置并添加自动修复功能

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,27 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { 
+     ignores: ['dist/**','others/**','public/**','build/**','node_modules/**','vite.config.js'], 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"],
+     rules: {
+      'no-unused-vars': 'off'  // 关闭未使用变量检查
+    }
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    languageOptions: { 
+      globals: globals.browser 
+    } 
+  },
+  {
+    // 扩展React推荐配置
+    ...pluginReact.configs.flat.recommended,
+    // 添加自定义规则
+    rules: {
+      'react/prop-types': 'off'
+    }
+  }
 ]);
